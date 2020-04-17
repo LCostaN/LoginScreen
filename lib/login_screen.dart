4ea8c425e35343afb2382c900290082f6bdf24a8
Lucas Nantes da Costa <nantes332@gmail.com>
2020-04-17 19:43:29 -0300

    Creating repository

---
+++ lib/login_screen.dart
@@ -0,0 +1,17 @@
+library login_screen;
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
